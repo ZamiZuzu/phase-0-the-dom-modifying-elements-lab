653b50d6a4d5cf5428868dfb1e8020dea830ff91
Christopher Valle <chrisbv@live.com>
2021-10-01 14:16:06 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+const main = document.getElementsByTagName("main")[0];
+main.remove();
+
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = "YOUR-NAME is the champion"
+
+document.body.append(newHeader);
+
+
+
